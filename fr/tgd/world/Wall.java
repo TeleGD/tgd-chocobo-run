62c98738602f64750b967f1f80cf8f5996adfbc2
valentin <valentindu71@gmail.com>
2015-03-06 22:29:02 +0100

    On a créé une classe Wall qui fait des murs a partir d'une borne Inf et Supp

---
+++ fr/tgd/world/Wall.java
@@ -0,0 +1,35 @@
+package fr.tgd.world;
+
+
+public class Wall {
+	private double inf,supp;
+	
+
+	public Wall(double inf , double supp) {
+		super();
+		this.inf = inf;
+		this.supp = supp;
+	}
+
+	public double getInf() {
+		return inf;
+	}
+
+
+	public void setInf(double inf) {
+		this.inf = inf;
+	}
+
+
+	public double getSupp() {
+		return supp;
+	}
+
+	public void setSupp(double supp) {
+		this.supp = supp;
+	}
+	
+	
+}
+
+

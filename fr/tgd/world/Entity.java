a2840b359c5968731a20877de4b19cd0e140d98a
valentin <valentindu71@gmail.com>
2015-03-06 21:39:31 +0100

    Ajout de Entity

---
+++ fr/tgd/world/Entity.java
@@ -0,0 +1,31 @@
+package fr.tgd.world;
+
+public abstract class Entity {
+	private double x, y;
+	
+	public Entity(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public abstract void update();
+	public abstract void render();
+	
+}

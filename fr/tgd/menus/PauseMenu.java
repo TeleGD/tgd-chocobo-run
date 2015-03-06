41c8322b321416431d7613fad1d73de4c86a0188
Adrien Wehrung <ad.game-adress@hotmail.fr>
2015-03-06 22:52:30 +0100

    nouveaux menus, encore non cliquables

--- fr/tgd/menus/PauseMenu.java
+++ fr/tgd/menus/PauseMenu.java
@@ -1,7 +1,32 @@
 package fr.tgd.menus;
 
-public class PauseMenu {
+import org.newdawn.slick.Graphics;
 
-	
+public class PauseMenu implements Scene {
+
+	private String nom = "Pause";
+	private String[] items = { "Continuer", "Recommencer", "Quitter" };
+
+	public PauseMenu() {}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		int x = 10;
+		int y = 30;
+		
+		g.drawString(this.nom, x, y);
+		
+		y += 40;
+		
+		for (String i : this.items) {
+			g.drawString(i, x, y);
+			y += 20;
+		}
+	}
 	
 }

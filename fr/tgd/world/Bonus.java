6eb6ab70c853db9f65f6f4f990fd56f60b28312b
Hverdung <elias.lorimy.tennis@hotmail.fr>
2015-03-07 05:23:47 +0100

    coucou

---
+++ fr/tgd/world/Bonus.java
@@ -0,0 +1,23 @@
+package fr.tgd.world;
+
+import java.util.Random;
+
+public class Bonus extends Circle {
+
+	public Bonus(double x, double y, int radius) {
+		super(x, y, radius);
+	}
+	
+	public void update(int delta){
+		
+	}
+	
+	public void render(int delta){
+		
+	}
+	
+	
+	
+	
+	
+}

9a558a33ea5b9efbb4e14fff3379ce5af6b13725
valentin <valentindu71@gmail.com>
2015-03-07 02:58:43 +0100

    On peut jouer , y a plein de  trous

--- fr/tgd/world/WallGenerator.java
+++ fr/tgd/world/WallGenerator.java
@@ -11,8 +11,8 @@ public class WallGenerator {
 	private static int minInterval=100 ;
 	private static int maxInterval=200 ;
 	public Random rand = new Random();
-	private double coef=0.000002;
-	private static int holeSize = 25 ; 
+	private double coef=0.00004;
+	private static int holeSize = 50 ; 
 	
 	public WallGenerator(World world) {
 		this.world= world;

713c65b3b3890a9c611898e73b124110a923bcc1
Adrien Wehrung <ad.game-adress@hotmail.fr>
2015-03-07 01:39:49 +0100

    menus non cliquables, curseur inclus

--- fr/tgd/menus/PauseMenu.java
+++ fr/tgd/menus/PauseMenu.java
@@ -1,31 +1,72 @@
 package fr.tgd.menus;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+
+import fr.tgd.util.FontManager;
 
 public class PauseMenu implements Scene {
-
 	private String nom = "Pause";
-	private String[] items = { "Continuer", "Recommencer", "Quitter" };
+	private String[] items = { "Continuer", "Quitter" };
 
-	public PauseMenu() {}
+	public int nbrOption = items.length;
+
+	public String[] getItems() {
+		return this.items;
+	}
+
+	static GameContainer container;
+	int selection = 0;
+
+	public PauseMenu() {
+
+	}
 
 	@Override
 	public void update() {
-		
+
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		int x = 10;
-		int y = 30;
-		
-		g.drawString(this.nom, x, y);
-		
-		y += 40;
-		
-		for (String i : this.items) {
-			g.drawString(i, x, y);
-			y += 20;
+
+		g.setColor(Color.blue);
+		g.setFont(FontManager.menuTitre1TTF);
+
+		g.drawString(this.nom, 200, 200);
+
+		g.setColor(Color.white);
+		g.setFont(FontManager.menuItemsTTF);
+
+		for (int i = 0; i < nbrOption; i++) {
+			g.drawString(this.items[i], 300, 280 + 50 * i);
+		}
+
+		g.drawString(">>", 230, 280 + 50 * selection);
+
+	}
+
+	@Override
+	public void keyPressed(int key) {
+		switch (key) {
+		case Input.KEY_DOWN:
+			if (selection < nbrOption - 1)
+				selection++;
+			else
+				selection = 0;
+			break;
+		case Input.KEY_UP:
+			if (selection > 0)
+				selection--;
+			else
+				selection = nbrOption - 1;
+			break;
+	/*  case Input.KEY_ENTER:
+			execOption();
+			break;
+		*/
 		}
 	}
 	

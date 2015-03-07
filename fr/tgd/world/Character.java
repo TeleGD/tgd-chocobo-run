3931bcaafa4b0da8bba98af6d4b67b368f6654df
Hverdung <elias.lorimy.tennis@hotmail.fr>
2015-03-07 04:48:42 +0100

    coucou

--- fr/tgd/world/Character.java
+++ fr/tgd/world/Character.java
@@ -76,7 +76,7 @@ public class Character extends Circle  {
 		
 	public void consumeStamina() {
 		if (isDash && stamina>=0){
-			stamina-=5;
+			stamina-=20;
 		}else {setIsDash(false);
 				setSpeedX(0.3f);
 		}

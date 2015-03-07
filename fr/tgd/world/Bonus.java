a17b83950fa0196122b249ba6e9dd4e451434c1a
Hverdung <elias.lorimy.tennis@hotmail.fr>
2015-03-07 05:50:47 +0100

    coucou

--- fr/tgd/world/Bonus.java
+++ fr/tgd/world/Bonus.java
@@ -2,6 +2,9 @@ package fr.tgd.world;
 
 import java.util.Random;
 
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+
 public class Bonus extends Circle {
 
 	public Bonus(double x, double y, int radius) {
@@ -10,11 +13,12 @@ public class Bonus extends Circle {
 	
 	
 	public void update(int delta){
-		
+		setY(getY()+Character.wallSpeed*delta);
 	}
 	
-	public void render(int delta){
-		
+	public void render(Graphics g){
+		g.setColor(Color.green);
+		g.drawOval((float) x-radius, (float) y-radius, 10, 10);
 	}
 	
 	
--- fr/tgd/world/BonusGenerator.java
+++ fr/tgd/world/BonusGenerator.java
@@ -6,8 +6,6 @@ import java.util.Random;
 public class BonusGenerator {
 	private World world ;
 	private int nextInterval,time ;
-	private static int minInterval=2000;
-	private static int maxInterval=5000;
 	public Random rand = new Random();
 	
 	public BonusGenerator(World world) {
